542b93d826ba9ef79ee47b9aa6d68df468b0b0c9
Alish Baig <alishbaig2004@gmail.com>
2025-01-20 20:13:54 +0530

    feat: remove unused CSS and integrate Tailwind CSS for styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Editor from "./editor";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex flex-col items-center justify-center w-full h-screen bg-gray-200">
+      <div className="mb-2 w-full max-w-5xl">
+        <h4 className="text-3xl font-extrabold text-left w-full">
+          Rich Text Editors
+        </h4>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="w-full max-w-5xl bg-white p-4">
+        <Editor />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
